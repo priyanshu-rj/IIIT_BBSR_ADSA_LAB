555c66bdf4e9875bedb759ca5bcd14a8a2c02904
priyanshu kumar <114394901+priyanshu-rj@users.noreply.github.com>
2025-09-21 00:39:34 +0530

    Update Strassen.c

--- ADSA_LAB_02/Question1/Strassen.c
+++ ADSA_LAB_02/Question1/Strassen.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-#define MAX 8   // Maximum matrix size (must be a power of 2)
+#include "munit.h"
 
-// Function to add two matrices
+#define MAX 8
+
+// ---------- Matrix Operations ----------
 void add(int n, int A[MAX][MAX], int B[MAX][MAX], int C[MAX][MAX]) {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             C[i][j] = A[i][j] + B[i][j];
 }
 
-// Function to subtract two matrices
 void sub(int n, int A[MAX][MAX], int B[MAX][MAX], int C[MAX][MAX]) {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             C[i][j] = A[i][j] - B[i][j];
 }
 
-// Recursive Strassen multiplication
+// ---------- Strassen Multiplication ----------
 void strassen(int n, int A[MAX][MAX], int B[MAX][MAX], int C[MAX][MAX]) {
     if (n == 2) {
         int a11 = A[0][0], a12 = A[0][1], a21 = A[1][0], a22 = A[1][1];
@@ -44,7 +45,7 @@ void strassen(int n, int A[MAX][MAX], int B[MAX][MAX], int C[MAX][MAX]) {
     int M5[MAX][MAX], M6[MAX][MAX], M7[MAX][MAX];
     int temp1[MAX][MAX], temp2[MAX][MAX];
 
-    // Split A and B into submatrices
+    // Split matrices
     for (int i = 0; i < k; i++) {
         for (int j = 0; j < k; j++) {
             A11[i][j] = A[i][j];
@@ -59,89 +60,124 @@ void strassen(int n, int A[MAX][MAX], int B[MAX][MAX], int C[MAX][MAX]) {
         }
     }
 
-    // M1 = (A11 + A22)(B11 + B22)
     add(k, A11, A22, temp1);
     add(k, B11, B22, temp2);
     strassen(k, temp1, temp2, M1);
 
-    // M2 = (A21 + A22) * B11
     add(k, A21, A22, temp1);
     strassen(k, temp1, B11, M2);
 
-    // M3 = A11 * (B12 - B22)
     sub(k, B12, B22, temp2);
     strassen(k, A11, temp2, M3);
 
-    // M4 = A22 * (B21 - B11)
     sub(k, B21, B11, temp2);
     strassen(k, A22, temp2, M4);
 
-    // M5 = (A11 + A12) * B22
     add(k, A11, A12, temp1);
     strassen(k, temp1, B22, M5);
 
-    // M6 = (A21 - A11) * (B11 + B12)
     sub(k, A21, A11, temp1);
     add(k, B11, B12, temp2);
     strassen(k, temp1, temp2, M6);
 
-    // M7 = (A12 - A22) * (B21 + B22)
     sub(k, A12, A22, temp1);
     add(k, B21, B22, temp2);
     strassen(k, temp1, temp2, M7);
 
-    // C11 = M1 + M4 - M5 + M7
     add(k, M1, M4, temp1);
     sub(k, temp1, M5, temp2);
     add(k, temp2, M7, C11);
 
-    // C12 = M3 + M5
     add(k, M3, M5, C12);
-
-    // C21 = M2 + M4
     add(k, M2, M4, C21);
 
-    // C22 = M1 - M2 + M3 + M6
     sub(k, M1, M2, temp1);
     add(k, temp1, M3, temp2);
     add(k, temp2, M6, C22);
 
-    // Combine submatrices into result C
+    // Combine results
     for (int i = 0; i < k; i++) {
         for (int j = 0; j < k; j++) {
-            C[i][j]       = C11[i][j];
-            C[i][j + k]   = C12[i][j];
-            C[i + k][j]   = C21[i][j];
-            C[i + k][j+k] = C22[i][j];
+            C[i][j] = C11[i][j];
+            C[i][j + k] = C12[i][j];
+            C[i + k][j] = C21[i][j];
+            C[i + k][j + k] = C22[i][j];
         }
     }
 }
 
-int main() {
-    int n;
-    int A[MAX][MAX], B[MAX][MAX], C[MAX][MAX];
-
-    printf("Enter matrix size (must be power of 2, <= %d): ", MAX);
-    scanf("%d", &n);
-
-    printf("Enter elements of matrix A (%dx%d):\n", n, n);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            scanf("%d", &A[i][j]);
-
-    printf("Enter elements of matrix B (%dx%d):\n", n, n);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            scanf("%d", &B[i][j]);
-
-    strassen(n, A, B, C);
-
-    printf("\nResult matrix C = A * B:\n");
+// ---------- Helper Function ----------
+void printMatrix(int n, int M[MAX][MAX]) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
-            printf("%d ", C[i][j]);
+            printf("%d ", M[i][j]);
         printf("\n");
     }
+}
 
-    return 0;
+// ---------- MUnit Test Cases ----------
+static MunitResult test_strassen_2x2(const MunitParameter params[], void* user_data) {
+    int A[MAX][MAX] = {{1, 2}, {3, 4}};
+    int B[MAX][MAX] = {{5, 6}, {7, 8}};
+    int C[MAX][MAX];
+
+    strassen(2, A, B, C);
+
+    int expected[MAX][MAX] = {{19, 22}, {43, 50}};
+    for(int i = 0; i < 2; i++)
+        for(int j = 0; j < 2; j++)
+            munit_assert_int(C[i][j], ==, expected[i][j]);
+
+    return MUNIT_OK;
+}
+
+static MunitResult test_strassen_4x4(const MunitParameter params[], void* user_data) {
+    int A[MAX][MAX] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16}
+    };
+    int B[MAX][MAX] = {
+        {16,15,14,13},
+        {12,11,10,9},
+        {8,7,6,5},
+        {4,3,2,1}
+    };
+    int C[MAX][MAX];
+
+    strassen(4, A, B, C);
+
+    int expected[MAX][MAX] = {
+        {80,70,60,50},
+        {240,214,188,162},
+        {400,358,316,274},
+        {560,502,444,386}
+    };
+
+    for(int i = 0; i < 4; i++)
+        for(int j = 0; j < 4; j++)
+            munit_assert_int(C[i][j], ==, expected[i][j]);
+
+    return MUNIT_OK;
+}
+
+// ---------- Test Suite ----------
+static MunitTest tests[] = {
+    {"/strassen_2x2", test_strassen_2x2, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+    {"/strassen_4x4", test_strassen_4x4, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+    {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
+};
+
+static const MunitSuite suite = {
+    "strassen-tests",
+    tests,
+    NULL,
+    1,
+    MUNIT_SUITE_OPTION_NONE
+};
+
+// ---------- Main ----------
+int main(int argc, char* argv[]) {
+    return munit_suite_main(&suite, NULL, argc, argv);
 }
